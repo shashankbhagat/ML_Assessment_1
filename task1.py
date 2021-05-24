91dac41c1fe707eb22654b6dd743a7c60593a0d1
Shashank Bhagat <bhagatshashank007@outlook.com>
2021-05-24 12:37:30 -0700

    product json flatten

--- task1.py
+++ task1.py
@@ -2,6 +2,7 @@ import json
 import datetime as dt
 import logging
 from django.core.serializers.json import DjangoJSONEncoder
+import traceback
 
 class Assessment:
     def __init__(self):
@@ -55,6 +56,7 @@ class Assessment:
 
         return
 
+    '''
     def format_products(self, ):
         self.logger.info("Start execution")
         try:
@@ -64,11 +66,101 @@ class Assessment:
             self.logger.error('Problem opening file. Check file path. '+ str(ex))
             return
         hierarchy_depth = len(json_data['report']['elements'])
+        elements=[]
+        for val in json_data['report']['elements']:
+            elements.append(val['name'])
         #use stack to go to deapest breakdown
+
+        output=[]
+        for packaged_data in json_data['report']['data']:
+            temp_hierarchy=[]
+            #traverse through all to collect the hierarchies except the last one.
+            #for i in range(hierarchy_depth-2):
+            #    temp_hierarchy.append(packaged_data['name'])
+            try:
+                stack=[packaged_data['name']]
+                while len(stack):
+                    stack_val=stack.pop()
+                    stack_val=stack_val.split(':')
+
+                    if len(stack_val)!=hierarchy_depth-1:
+                        temp_data = packaged_data
+                        temp_data = temp_data['breakdown']
+                        print(stack_val, temp_data)
+                        print(len(temp_data))
+                        while len(temp_data):
+                            temp_data_val=temp_data.pop()
+                            temp_stackval=stack_val
+                            temp_stackval.append(temp_data_val['name'])
+                            stack.append(':'.join(temp_stackval))
+
+                    else:
+                        temp_dict = {}
+                        print('in else: ',temp_data)
+                        temp_dict['page_views'] = temp_data['counts'][0]
+                        temp_dict['visits'] = temp_data['counts'][1]
+                        for i in range(len(stack_val)):
+                            temp_dict[elements[i]] = stack_val[i]
+                        output.append(temp_dict)
+
+            except Exception as ex:
+                self.logger.error("Problem processing data. "+str(ex) + traceback.print_exc())
+
+        print(output)
+        with open('products_output.json', 'w') as f:
+            json.dump(output, f)
+        self.logger.info("Excution complete")
 
 
         return
+    '''
+
+    def format_products(self, ):
+
+        output=[]
+        elements = []
+
+        def flatten(data, name=''):
+            try:
+                if type(data) is list:
+                    i = 0
+                    for val in data:
+                        if 'breakdown' in val:
+                            flatten(val['breakdown'], name + val['name'] + '::')
+                        else:
+                            out={}
+
+                            temp_name=name+val['name']
+                            temp_name = temp_name.split('::')
+                            temp_name=list(filter(None, temp_name))
+                            #print(temp_name)
+                            for i,value in enumerate(temp_name):
+                                out[elements[i]]=value
+                            out['page_views'] = val['counts'][0]
+                            out['visits'] = val['counts'][1]
+                            output.append(out)
+            except Exception as ex:
+                self.logger.error('Problem processing data. '+str(ex)+traceback.print_exc())
+
+        try:
+            with open(self.products_path) as f:
+                json_data = json.load(f)
+        except Exception as ex:
+            self.logger.error('Problem opening file. Check file path. ' + str(ex))
+            return
+
+        for val in json_data['report']['elements']:
+            elements.append(val['name'])
+
+        flatten(json_data['report']['data'])
+        print(output)
+        with open('products_output.json', 'w') as f:
+            json.dump(output, f)
+        self.logger.info("Excution complete")
+        return output
 
 
 obj=Assessment()
-obj.format_key_metrics()
+#obj.format_key_metrics()
+obj.format_products()
+
