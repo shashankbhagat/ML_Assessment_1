c2a65fba657dc2863b3ea4bc96259a6552a788df
Shashank Bhagat <bhagatshashank007@outlook.com>
2021-05-24 16:13:45 -0700

    task 3

--- Assessment.py
+++ Assessment.py
@@ -1,8 +1,11 @@
 import json
 import datetime as dt
 import logging
+import sys
+
 from django.core.serializers.json import DjangoJSONEncoder
 import traceback
+import os
 
 class Assessment:
     def __init__(self):
@@ -14,6 +17,7 @@ class Assessment:
 
     def format_key_metrics(self, ):
         try:
+            #verifying if input json exists
             with open(self.key_metrics_path) as f:
                 json_data=json.load(f)
         except Exception as ex:
@@ -22,7 +26,8 @@ class Assessment:
         output=[]
 
         self.logger.info('Creating metrics dynamically')
-        #The metrics dict would prove efficient if new key metrics are added
+        #The metrics list would prove efficient if new key metrics are added
+        #collecting all key metrics along with their type for processing
         metrics=[]
         for i,metric in enumerate(json_data['report']['metrics']):
             metrics.append((metric['id'],metric['type']))
@@ -30,12 +35,14 @@ class Assessment:
         for val in json_data['report']['data']:
             temp={}
             try:
+                #creating datetime object
                 dt_obj=dt.datetime(year=val['year'],month=val['month'],day=val['day'])
                 temp['Date']=dt_obj
             except Exception as ex:
                 self.logger.error('Problem creating datetime object. '+ str(ex))
                 continue
 
+            #iterating on key metrics available and converting them depending on type.
             for i,metric in enumerate(metrics):
                 try:
                     if metric[1]=='number':
@@ -56,75 +63,18 @@ class Assessment:
 
         return
 
-    '''
-    def format_products(self, ):
-        self.logger.info("Start execution")
-        try:
-            with open(self.products_path) as f:
-                json_data=json.load(f)
-        except Exception as ex:
-            self.logger.error('Problem opening file. Check file path. '+ str(ex))
-            return
-        hierarchy_depth = len(json_data['report']['elements'])
-        elements=[]
-        for val in json_data['report']['elements']:
-            elements.append(val['name'])
-        #use stack to go to deapest breakdown
-
-        output=[]
-        for packaged_data in json_data['report']['data']:
-            temp_hierarchy=[]
-            #traverse through all to collect the hierarchies except the last one.
-            #for i in range(hierarchy_depth-2):
-            #    temp_hierarchy.append(packaged_data['name'])
-            try:
-                stack=[packaged_data['name']]
-                while len(stack):
-                    stack_val=stack.pop()
-                    stack_val=stack_val.split(':')
-
-                    if len(stack_val)!=hierarchy_depth-1:
-                        temp_data = packaged_data
-                        temp_data = temp_data['breakdown']
-                        print(stack_val, temp_data)
-                        print(len(temp_data))
-                        while len(temp_data):
-                            temp_data_val=temp_data.pop()
-                            temp_stackval=stack_val
-                            temp_stackval.append(temp_data_val['name'])
-                            stack.append(':'.join(temp_stackval))
-
-                    else:
-                        temp_dict = {}
-                        print('in else: ',temp_data)
-                        temp_dict['page_views'] = temp_data['counts'][0]
-                        temp_dict['visits'] = temp_data['counts'][1]
-                        for i in range(len(stack_val)):
-                            temp_dict[elements[i]] = stack_val[i]
-                        output.append(temp_dict)
-
-            except Exception as ex:
-                self.logger.error("Problem processing data. "+str(ex) + traceback.print_exc())
-
-        print(output)
-        with open('products_output.json', 'w') as f:
-            json.dump(output, f)
-        self.logger.info("Excution complete")
-
-
-        return
-    '''
 
     def format_products(self, ):
-
         output=[]
         elements = []
 
+        #recursive call to this function to fetch hierarchy in the json input
         def flatten(data, name=''):
             try:
                 if type(data) is list:
                     i = 0
                     for val in data:
+                        # verifying the hierarchy if exists and appending the names with :: placeholder
                         if 'breakdown' in val:
                             flatten(val['breakdown'], name + val['name'] + '::')
                         else:
@@ -132,15 +82,17 @@ class Assessment:
 
                             temp_name=name+val['name']
                             temp_name = temp_name.split('::')
+                            #filtering empty and null values
                             temp_name=list(filter(None, temp_name))
-                            #print(temp_name)
+
+                            #inner most hierarchy. collecting data.
                             for i,value in enumerate(temp_name):
                                 out[elements[i]]=value
                             out['page_views'] = val['counts'][0]
                             out['visits'] = val['counts'][1]
                             output.append(out)
             except Exception as ex:
-                self.logger.error('Problem processing data. '+str(ex)+traceback.print_exc())
+                self.logger.error('Problem processing data. '+str(ex)+traceback.format_exc())
 
         try:
             with open(self.products_path) as f:
@@ -149,6 +101,7 @@ class Assessment:
             self.logger.error('Problem opening file. Check file path. ' + str(ex))
             return
 
+        #collecting all key elements present to make it dynamic
         for val in json_data['report']['elements']:
             elements.append(val['name'])
 
@@ -156,11 +109,19 @@ class Assessment:
         print(output)
         with open('products_output.json', 'w') as f:
             json.dump(output, f)
-        self.logger.info("Excution complete")
+        self.logger.info("Execution complete")
         return output
 
 
-obj=Assessment()
-#obj.format_key_metrics()
-obj.format_products()
+if __name__=='__main__':
+    obj = Assessment()
+    logging.info("If no parameter is sent, then by default task-1 is executed")
+    if len(sys.argv)<2:
+        obj.format_key_metrics()
+    elif sys.argv[1]=='task1':
+        obj.format_key_metrics()
+    elif sys.argv[1]=='task2':
+        obj.format_products()
+    else:
+        logging.error("Incorrect parameter: "+sys.argv[1])
 
---
+++ README.txt
@@ -0,0 +1,15 @@
+To run the file , on the command line
+Assessment.py [task1/task2]
+
+By default, if no parameter is sent, the code for Task-1 is executed.
+Else, task1 -> executes code for Task-1
+task2 -> executes code for Task-2
+
+The code would generate output on the console as well as in a json file.
+Task-1 output -> key_metric_output.json
+Task-2 output -> products_output.json
+
+The description and detailed analysis for Task-3 is present in the file Task-3.txt
+
+Please Note: For simplicity, please place the input json files in the same directory as the python file.
+For simplicity, the file name is kept the same in the code as they were sent.
